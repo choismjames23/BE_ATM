f566c560a9790198e2cfbb588b16ab02e8528ed9
choismjames23 <choism23@naver.com>
2025-03-12 18:05:58 +0900

    ✨ 입금 기능 추가
    - 입력받은 값이 숫자만 존재하는지, 0보다 큰 값인지를 동시에 만족하족할 때만 입금이 가능하도록 구현.
    - 만족하지 않으면 숫자 형태가 아님을 알림.
    - 입력받은 값을 원래 잔고인 balance에 더함

--- atm.py
+++ atm.py
@@ -5,15 +5,21 @@ balance = 3000
 
 while True:
     #원하는 기능을 입력후 정수 형태로 변환.(입급:1, 출금:2, 영수증보기:3, 종료:0)
-    mode = int(input("원하는 기능의 번호를 입력하세요.(입급:1, 출금:2, 영수증보기:3, 종료:0) : "))
+    mode = int(input("원하는 기능의 번호를 입력하세요.(입금:1, 출금:2, 영수증보기:3, 종료:0) : "))
 
     if mode == 0:
+        print("서비스를 종료합니다.")
         break
     if mode == 1:
-        pass
+        deposit_amount = input("입금 금액 입력: ")
+        # .isdigit() => 문자열 안에 숫자만 존재하는지 확인하는 메소드. 맞으면 True 반환
+        if deposit_amount.isdigit() and int(deposit_amount) > 0:
+            balance += int(deposit_amount)
+            print(f'{deposit_amount}원 입금되었습니다. 현재 잔고는 {balance}원 입니다.')
+        else:
+            print("숫자 형태가 아닙니다.")
+
     if mode == 2:
         pass
     if mode == 3:
         pass
-
-print(mode)
